43d894b07f33af06231ccf5de5f0295279e4d72a
Dmitry <Dovgan.D17@yandex.ru>
2018-02-19 21:15:55 +0300

    [0.2] Optimization and normal work

--- core/main.cpp
+++ core/main.cpp
@@ -6,15 +6,19 @@ using std::endl;
 using std::string;
 
           int main(){
-  string first_text[] = {"Enter text: ", "Enter value: "};
-  string text = "";
+  string first_text[] = {"Enter text: ", "Enter value: ", "Value is not corrected!"};
+   string text = "";
   int value = 0;
   cout<<first_text[0];
   cin >>text;
   cout <<first_text[1];
   cin >> value;
-  for(int i = 0; i < value; i++){
+  if(value != 0){
+     for(int i = 0; i < value; i++){
   cout << text<<endl;
+}
+}else{
+     cout<<first_text[2];
 }
   return 0;
 }
